ab6327d2f8979c49f731e04ec4e71cbd3b2e47c1
brams1123 <bramsosifo@gmail.com>
2016-10-15 13:57:53 -0400

    Add files via upload

---
+++ Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+    float maxSpeed = 5f;
+    float rotSpeed = 180f;
+
+    float shipBoundRad = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Rotation Movement
+
+        Quaternion rot = transform.rotation;
+        float z = rot.eulerAngles.z;
+        z -= Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime;
+        rot = Quaternion.Euler(0, 0, z);
+        transform.rotation = rot;
+
+
+        //Up and Down Movement 
+
+        Vector3 pos = transform.position;
+        Vector3 velocity = new Vector3(0, Input.GetAxis("Vertical") * maxSpeed * Time.deltaTime, 0);
+        pos += rot * velocity;
+
+        //Ship Boundary inside of Main Camera
+        if(pos.y+shipBoundRad > Camera.main.orthographicSize)
+        {
+            pos.y = Camera.main.orthographicSize - shipBoundRad;
+        }
+        if (pos.y-shipBoundRad < -Camera.main.orthographicSize)
+        {
+            pos.y = -Camera.main.orthographicSize + shipBoundRad;
+        }
+
+        float Scnratio = (float)Screen.width / (float)Screen.height;
+        float widthOrtho = Camera.main.orthographicSize * Scnratio;
+
+        if (pos.x+shipBoundRad > widthOrtho)
+        {
+            pos.x = widthOrtho - shipBoundRad;
+        }
+
+        if (pos.x-shipBoundRad < -widthOrtho)
+        {
+            pos.x = -widthOrtho + shipBoundRad;
+        }
+        transform.position = pos;
+
+        
+	
+	}
+}
---
+++ Assets/_Scripts/PlayerShooting.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShooting : MonoBehaviour {
+    // Update is called once per frame
+    public GameObject bulletPrefab;
+
+    public float fireDelay = 0.25f;
+    float cooldownTimer = 0;
+
+	void Update () {
+        cooldownTimer -= Time.deltaTime;
+        if(Input.GetButton("Fire1") && cooldownTimer <= 0)
+        {
+            Debug.Log("Pew!");
+            cooldownTimer = fireDelay;
+
+            Instantiate(bulletPrefab, transform.position, transform.rotation);
+        }
+	
+	}
+}
